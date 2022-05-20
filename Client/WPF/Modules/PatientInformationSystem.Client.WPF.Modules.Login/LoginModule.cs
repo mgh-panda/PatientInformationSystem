5690a05ca5980ffa22095a4c139f04c8e13b3c7d
Michael Hamilton <mghamilton.panda@gmail.com>
2022-05-20 14:38:03 -0500

    Initial solution structure

---
+++ Client/WPF/Modules/PatientInformationSystem.Client.WPF.Modules.Login/LoginModule.cs
@@ -0,0 +1,20 @@
+﻿using PatientInformationSystem.Client.WPF.Modules.Login.Views;
+using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace PatientInformationSystem.Client.WPF.Modules.Login
+{
+    public class LoginModule : IModule
+    {
+        public void OnInitialized(IContainerProvider containerProvider)
+        {
+
+        }
+
+        public void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+
+        }
+    }
+}
---
+++ Client/WPF/PatientInformationSystem.Client.WPF.PrismApp/Views/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace PatientInformationSystem.Client.WPF.PrismApp.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
