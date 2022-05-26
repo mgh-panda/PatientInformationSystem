3bc544fecb79f431384d39d42517ce6010711929
Michael Hamilton <mghamilton.panda@gmail.com>
2022-05-26 09:17:27 -0500

    Add NewEmployeeRequest Fix possible null value warnings

--- Client/PatientInformationSystem.Client.Core/Models/APIModels/Authentication/AuthenticationResponse.cs
+++ Client/PatientInformationSystem.Client.Core/Models/APIModels/Authentication/AuthenticationResponse.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PatientInformationSystem.Client.Core.Models.APIModels.Employee;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
--- Client/PatientInformationSystem.Client.Core/Models/APIModels/Employee/NewEmployeeRequest.cs
+++ Client/PatientInformationSystem.Client.Core/Models/APIModels/Employee/NewEmployeeRequest.cs
@@ -1,4 +1,10 @@
-﻿namespace PatientInformationSystem.Server.Application.Models.Employee
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatientInformationSystem.Client.Core.Models.APIModels.Employee
 {
     public record NewEmployeeRequest
     {
